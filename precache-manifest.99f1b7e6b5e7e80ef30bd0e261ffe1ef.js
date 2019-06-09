bd44edcccf519aad000cb229695f8b01b05723bc
jkcon <github@xiixii.pw>
2019-06-09 13:25:53 -0500

    Updates

---
+++ precache-manifest.99f1b7e6b5e7e80ef30bd0e261ffe1ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e230f9dd8be4449bffb570dad48af3",
+    "url": "/take-home/index.html"
+  },
+  {
+    "revision": "516c5d8d67b9456ee3e7",
+    "url": "/take-home/static/css/main.b9c00197.chunk.css"
+  },
+  {
+    "revision": "076a9d6424a29c7f7423",
+    "url": "/take-home/static/js/2.2aaeeb6b.chunk.js"
+  },
+  {
+    "revision": "516c5d8d67b9456ee3e7",
+    "url": "/take-home/static/js/main.94169a2d.chunk.js"
+  },
+  {
+    "revision": "345f4bd667772e676302",
+    "url": "/take-home/static/js/runtime~main.36d1acd5.js"
+  }
+]);
