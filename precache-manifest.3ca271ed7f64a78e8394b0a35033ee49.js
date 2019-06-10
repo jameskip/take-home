6978af3e8ffd190ecac8e58b5195ecd24bf5fab6
jkcon <github@xiixii.pw>
2019-06-10 13:32:42 -0500

    Updates

--- precache-manifest.3ca271ed7f64a78e8394b0a35033ee49.js
+++ precache-manifest.3ca271ed7f64a78e8394b0a35033ee49.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4adf830645b1738edfd3100f134b2cfb",
+    "revision": "db11592d77fa0a8740ebbe9b09034e7a",
     "url": "/take-home/index.html"
   },
   {
-    "revision": "8a4a01b5843634dded0c",
+    "revision": "bbef6757189a682aaf53",
     "url": "/take-home/static/css/main.b9c00197.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/take-home/static/js/2.ce9ab442.chunk.js"
   },
   {
-    "revision": "8a4a01b5843634dded0c",
-    "url": "/take-home/static/js/main.e3d40891.chunk.js"
+    "revision": "bbef6757189a682aaf53",
+    "url": "/take-home/static/js/main.fe569b0c.chunk.js"
   },
   {
     "revision": "345f4bd667772e676302",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/take-home/precache-manifest.fd08312cb974ff39e9d368233c92dfd6.js"
+  "/take-home/precache-manifest.3ca271ed7f64a78e8394b0a35033ee49.js"
 );
 
 self.addEventListener('message', (event) => {
