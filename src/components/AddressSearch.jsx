7f10b269a3839e60674273476cd2164b14c031df
jkcon <github@xiixii.pw>
2019-06-10 09:17:32 -0500

    Add responsive styling to search

--- src/components/AddressSearch.jsx
+++ src/components/AddressSearch.jsx
@@ -8,6 +8,7 @@ import Button from '@material-ui/core/Button' // eslint-disable-line no-unused-v
 import TextField from '@material-ui/core/TextField' // eslint-disable-line no-unused-vars
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container' // eslint-disable-line no-unused-vars
+import Grid from '@material-ui/core/Grid'
 
 import ConfirmAddress from './ConfirmAddress' // eslint-disable-line no-unused-vars
 import { addOrigin, addDestination, checkAddress } from '../redux/actions'
@@ -16,18 +17,20 @@ const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(3, 2),
     position: 'absolute',
-    top: '25%'
+    top: '25%',
+    flexGrow: 1
   },
   container: {
     height: '100%',
+    width: '100%',
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    textAlign: 'center'
   },
   textField: {
     marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: 400
+    marginRight: theme.spacing(1)
   },
   button: {
     margin: theme.spacing(1),
@@ -55,40 +58,41 @@ const AddressSearch = (props: Props) => {
   const disableButton = () => state.addressReducer.originAddress.length < 1 || state.addressReducer.destinationAddress < 1 ? setDisabled(true) : setDisabled(false)
 
   return (
+    <Grid container spacing={2}>
+      <div className={classes.container}>
+        <Paper className={classes.root}>
 
-    <div className={classes.container}>
-      <Paper className={classes.root}>
+          <TextField
+            required
+            id="standard-required"
+            label="From"
+            value={state.originAddress}
+            onChange={handleChange('origin')}
+            className={classes.textField}
+            margin="normal"
+          />
 
-        <TextField
-          required
-          id="standard-required"
-          label="From"
-          value={state.originAddress}
-          onChange={handleChange('origin')}
-          className={classes.textField}
-          margin="normal"
-        />
-
-        <TextField
-          required
-          id="standard-required"
-          label="To"
-          value={state.destinationAddress}
-          onChange={handleChange('destination')}
-          className={classes.textField}
-          margin="normal"
-        />
-
-        <Button
-          onClick={handleClick} to='/confirm'
-          component={Link} className={classes.button}
-          disabled={disabled}
-        >
+          <TextField
+            required
+            id="standard-required"
+            label="To"
+            value={state.destinationAddress}
+            onChange={handleChange('destination')}
+            className={classes.textField}
+            margin="normal"
+          />
+          <Grid item xs={12}>
+            <Button
+              onClick={handleClick} to='/confirm'
+              component={Link} className={classes.button}
+              disabled={disabled}
+            >
             Search
-        </Button>
-
-      </Paper>
-    </div>
+            </Button>
+          </Grid>
+        </Paper>
+      </div>
+    </Grid>
   )
 }
 
